d6725d1e7c73987b244c8d629ff368f4ce62e6f4
Ivan Schaller <ivan@schaller.sh>
2022-05-10 16:38:14 +0200

    update tests and add coverage report

--- tests/test_api_mangadex.py
+++ tests/test_api_mangadex.py
@@ -1,18 +1,47 @@
-import pytest
 from mangadlp.api.mangadex import Mangadex
 
-def test_uuid():
-  url = 'https://mangadex.org/title/a96676e5-8ae2-425e-b549-7f15dd34a6d8/komi-san-wa-komyushou-desu'
-  lang = 'en'
-  test = Mangadex(url, lang)
-  assert test.get_manga_uuid() == 'a96676e5-8ae2-425e-b549-7f15dd34a6d8'
+
+def test_uuid_link():
+    url = "https://mangadex.org/title/a96676e5-8ae2-425e-b549-7f15dd34a6d8/komi-san-wa-komyushou-desu"
+    lang = "en"
+    forcevol = False
+    verbose = False
+    test = Mangadex(url, lang, forcevol, verbose)
+    assert test.manga_uuid == "a96676e5-8ae2-425e-b549-7f15dd34a6d8"
+
+
+def test_uuid_pure():
+    url = "a96676e5-8ae2-425e-b549-7f15dd34a6d8"
+    lang = "en"
+    forcevol = False
+    verbose = False
+    test = Mangadex(url, lang, forcevol, verbose)
+    assert test.manga_uuid == "a96676e5-8ae2-425e-b549-7f15dd34a6d8"
 
 
 def test_title():
-  url = 'https://mangadex.org/title/a96676e5-8ae2-425e-b549-7f15dd34a6d8/komi-san-wa-komyushou-desu'
-  lang = 'en'
-  test = Mangadex(url, lang)
-  title = test.get_manga_title(test.get_manga_uuid())
-  assert title == 'Komi-san wa Komyushou Desu'
+    url = "https://mangadex.org/title/a96676e5-8ae2-425e-b549-7f15dd34a6d8/komi-san-wa-komyushou-desu"
+    lang = "en"
+    forcevol = False
+    verbose = False
+    test = Mangadex(url, lang, forcevol, verbose)
+    assert test.manga_title == "Komi-san wa Komyushou Desu"
 
 
+def test_chapter_infos():
+    url = "https://mangadex.org/title/a96676e5-8ae2-425e-b549-7f15dd34a6d8/komi-san-wa-komyushou-desu"
+    lang = "en"
+    forcevol = False
+    verbose = False
+    test = Mangadex(url, lang, forcevol, verbose)
+    chapter_infos = test.get_chapter_infos("1")
+    chapter_uuid = chapter_infos["uuid"]
+    chapter_name = chapter_infos["name"]
+    chapter_num = chapter_infos["chapter"]
+    chapter_volume = chapter_infos["volume"]
+    assert [chapter_uuid, chapter_name, chapter_volume, chapter_num] == [
+        "e86ec2c4-c5e4-4710-bfaa-7604f00939c7",
+        "A Normal Person",
+        "1",
+        "1",
+    ]
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,20 @@
-import pytest
 from pathlib import Path
 import mangadlp.main as MdlpMain
-import mangadlp.utils as MdlpUtils
-import mangadlp.downloader as MdlpDownloader
+
 
 def test_readin_list():
-  list_file = Path('test.txt')
-  test_list = MdlpMain.readin_list(list_file)
+    list_file = Path("tests/test_list.txt")
+    test_list = MdlpMain.readin_list(list_file)
 
-  assert test_list == ['https://mangadex.org/title/a96676e5-8ae2-425e-b549-7f15dd34a6d8/komi-san-wa-komyushou-desu', 'https://mangadex.org/title/bd6d0982-0091-4945-ad70-c028ed3c0917/mushoku-tensei-isekai-ittara-honki-dasu']
+    assert test_list == [
+        "https://mangadex.org/title/a96676e5-8ae2-425e-b549-7f15dd34a6d8/komi-san-wa-komyushou-desu",
+        "https://mangadex.org/title/bd6d0982-0091-4945-ad70-c028ed3c0917/mushoku-tensei-isekai-ittara-honki-dasu",
+        "37f5cce0-8070-4ada-96e5-fa24b1bd4ff9",
+    ]
 
 
 def check_api():
-  url = 'https://mangadex.org/title/a96676e5-8ae2-425e-b549-7f15dd34a6d8/komi-san-wa-komyushou-desu'
-  test = MdlpMain.check_api(url)
+    url = "https://mangadex.org/title/a96676e5-8ae2-425e-b549-7f15dd34a6d8/komi-san-wa-komyushou-desu"
+    test = MdlpMain.check_api(url)
 
-  assert test == eval(mangadlp.api.mangadex.Mangadex)
+    assert test == eval(mangadlp.api.mangadex.Mangadex)
