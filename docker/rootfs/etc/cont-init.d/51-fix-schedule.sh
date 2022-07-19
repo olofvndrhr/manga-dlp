fc3f1984a3bdf90982f78008e2b3b5d83c3c748d
Ivan Schaller <ivan@schaller.sh>
2022-07-19 22:45:00 +0200

    fix schedule fixer

--- docker/rootfs/etc/cont-init.d/51-fix-schedule.sh
+++ docker/rootfs/etc/cont-init.d/51-fix-schedule.sh
@@ -5,23 +5,25 @@
 source /etc/cont-init.d/20-setenv.sh
 
 # check schedule
-[[ -f "/app/schedules/daily.sh" ]] && DAILYSH=true
-[[ -f "/app/schedules/daily" ]] && DAILY=true
+[[ -f "/app/schedules/daily.sh" ]] && DAILY_SH_FOUND=true
+[[ -f "/app/schedules/daily" ]] && DAILY_FOUND=true
 # check crontab
 if grep -q -e "/app/schedules/daily.sh\s" /etc/cron.d/mangadlp; then
-    CRONSH=true
+    CRON_SH_FOUND=true
 elif grep -q -e "/app/schedules/daily\s" /etc/cron.d/mangadlp; then
-    CRON=true
+    CRON_FOUND=true
 fi
 
 # fix new .sh schedule if its not synced with the crontab
-if [[ "${CRONSH}" == "true" ]] && [[ "${DAILYSH}" != "true" ]]; then
+if [[ "${CRON_SH_FOUND}" == "true" ]] && [[ "${DAILY_SH_FOUND}" != "true" ]]; then
     echo "Fixing new .sh schedule"
+    echo "Adding symlink to daily.sh"
     if ! ln -s /app/schedule/daily /app/schedule/daily.sh; then
         echo "Cant fix schedule. Maybe the file is missing."
     fi
-elif [[ "${CRON}" == "true" ]] && [[ "${DAILY}" != "true" ]]; then
+elif [[ "${CRON_FOUND}" == "true" ]] && [[ "${DAILY_FOUND}" != "true" ]]; then
     echo "Fixing new .sh schedule"
+    echo "Adding symlink to daily"
     if ! ln -s /app/schedule/daily.sh /app/schedule/daily; then
         echo "Cant fix schedule. Maybe the file is missing."
     fi
